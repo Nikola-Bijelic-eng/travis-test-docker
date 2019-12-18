a2a4d55d1ebcb5a7e27436c9f0449ef09002396d
Nikola-Bijelic <nikolabijelic14@gmail.com>
2019-12-18 12:47:19 +0100

    new test

---
+++ wait-for-it/test.sh
@@ -0,0 +1,6 @@
+- curl -X POST \
+  http://localhost:8443/incoming-data-app/MultipartMessage \ 
+  -H 'Accept: */*' \
+  -H 'Content-Type: multipart/mixed; boundary=CQWZRdCCXr5aIuonjmRXF-QzcZ2Kyi4Dkn6;charset=UTF-8' \
+  -H 'Forward-To: https://localhost:8444/incoming-data-channel/receivedMessage' \
+  -D 'MULTIPART MESSAGE DATA' 
