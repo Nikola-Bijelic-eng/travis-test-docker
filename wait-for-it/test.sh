20aaf1f087641e75fc7a674f55470100a4b4228b
Nikola-Bijelic <nikolabijelic14@gmail.com>
2020-01-10 11:06:12 +0100

    new bash build

--- wait-for-it/test.sh
+++ wait-for-it/test.sh
@@ -1,7 +1,1 @@
-#  echoerr "WELOOOOOOOO WOOOORLLLLDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
-# - curl -X POST \
-#   http://localhost:8443/incoming-data-app/MultipartMessage \ 
-#   -H 'Accept: */*' \
-#   -H 'Content-Type: multipart/mixed; boundary=CQWZRdCCXr5aIuonjmRXF-QzcZ2Kyi4Dkn6;charset=UTF-8' \
-#   -H 'Forward-To: https://localhost:8444/incoming-data-channel/receivedMessage' \
-#   -D 'MULTIPART MESSAGE DATA' 
+curl -k -request GET 'https://localhost:8450/about/version'
